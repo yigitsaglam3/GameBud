16ce7a209f0600ef8765995d9ca106e785399bd3
dogger2304 <lyigitsaglam@gmail.com>
2024-03-11 12:26:12 +0300

    first commit

---
+++ gamebud/src/main/java/com/proje/gamebud/GamebudApplication.java
@@ -0,0 +1,13 @@
+package com.proje.gamebud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebudApplication.class, args);
+	}
+
+}
